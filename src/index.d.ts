fd6934aa8e079177a97a9e91622fb1fad57d50d4
parisame <73756399+parisame@users.noreply.github.com>
2023-09-18 15:26:42 +0000

    fix on definition

--- src/index.d.ts
+++ src/index.d.ts
@@ -66,7 +66,7 @@ declare namespace CalHeatmap {
     destroy: () => Promise<unknown>;
   }
   export interface IPluginContructor {
-    new (calendar?: CalHeatmap): IPlugin;
+    new(calendar?: CalHeatmap): IPlugin;
   }
 
   export interface PluginOptions {
@@ -95,7 +95,7 @@ declare class CalHeatmap {
 
   fill(dataSource?: OptionsType['data']['source']): Promise<unknown>;
 
-  on(name: string, fn: () => any): void;
+  on(name: string, fn: (event: Event, timestamp: CalHeatmap.Timestamp, value: any) => any): void;
 
   dimensions(): CalHeatmap.Dimensions;
 
