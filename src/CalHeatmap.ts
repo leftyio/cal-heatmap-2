5ed4baeae3f364abdcbafd804bedec7b1c6881d6
parisame <73756399+parisame@users.noreply.github.com>
2023-09-19 15:35:55 +0000

    test sth

--- src/CalHeatmap.ts
+++ src/CalHeatmap.ts
@@ -248,7 +248,7 @@ export default class CalHeatmap {
    * @param  {function} Callback function to execute on event trigger
    * @return void
    */
-  on(name: string, fn: () => any): void {
+  on(name: string, fn: (event: Event, timestamp: CalHeatmap.Timestamp, value: any) => any): void {
     this.eventEmitter.on(name, fn);
   }
 
